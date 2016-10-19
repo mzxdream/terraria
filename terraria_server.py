501793d064abe58533704843e336af9ce2fd095b
mzxdream <mzxdream@gmail.com>
2016-10-19 22:20:38 +0800

    1

--- terraria_server.py
+++ terraria_server.py
@@ -18,41 +18,46 @@ nat_port = 7777
 header_size = 4
 
 HEART_BEAT = 0
-G2N_REGISTE_ASK = 1
-N2G_REGISTE_RET = 2
-N2G_CONNECT_ASK = 3
-G2N_CONNECT_RET = 4
-G2N_CONNECT_ASK = 5
-N2G_CONNECT_RET = 6
+
+S2N_REGISTE_SERV_ASK = 1
+N2S_REGISTE_SERV_RET = 2
+
+N2S_CLIENT_CONNECT_ASK = 3
+S2N_CLIENT_CONNECT_RET = 4
+
+S2N_CONNECT_CLIENT_ASK = 5
+N2S_CONNECT_CLIENT_RET = 6
 
 class ProxyLocalClient(asyncore.dispatcher):
     pass
 
 class ProxyRemoteClient(asyncore.dispatcher):
-    def __init__(self, key):
+    def __init__(self, handler, key):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(("", 0))
         self.connect((nat_host, nat_port))
         self.send_buffer = bytes()
         self.recv_buffer = bytes()
         self.status = "connecting"
         self.connect_client = False
         self.last_time = time.time()
+        self.handler = handler
         self.key = key
         self.client_addr = None
-        self.client_port = None
         self.client = None
 
     def handle_connect(self):
         self.status = "connected"
         self.last_time = time.time()
         if not self.connect_client:
-            cmd = G2N_CONNECT_ASK
+            cmd = S2N_CONNECT_CLIENT_ASK
             self.send_buffer += (struct.pack("!2H", len(self.key), cmd) + self.key)
             print "connect nat server"
         else:
             if self.client:
-                self.client.close_proxy()
+                self.client.close()
             self.client = ProxyLocalClient(self)
             self.send_buffer = bytes()
             self.recv_buffer = bytes()
@@ -105,8 +110,18 @@ class ProxyRemoteClient(asyncore.dispatcher):
             self.handle_cmd(cmd, body)
 
     def handle_cmd(self, cmd, body):
-        if cmd == N2G_CONNECT_ASK:
-            self.handle_new_connect(body)
+        if not self.connect_client:
+            if cmd == N2S_CONNECT_CLIENT_RET:
+                addr = body.split(":")
+                if addr != 2:
+                    self.close()
+                    self.handler.close_connect(self.key)
+                    return
+                self.client_addr = (addr[0], int(addr[1]))
+                addr = self.socket.getsockname()
+                self.close()
+
+
 
     def handle_new_connect(self, key):
         self.clients[key] = ProxyRemoteClient(self, key)
