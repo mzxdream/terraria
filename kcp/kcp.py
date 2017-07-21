2fb61f20886f68e9edacd0a0b50780c032bad1c4
mzxdream <mzxdream@gmail.com>
2017-07-21 18:01:21 +0800

    1

--- kcp/kcp.py
+++ kcp/kcp.py
@@ -66,83 +66,33 @@ class Kcp(object):
         if size <= 0:
             raise ValueError("size:%s is less than 1" % (size))
 
-        for segment in self._rcv_buf:
+        while len(data) < size and len(self._rcv_buf) > 0:
+            segment = self._rcv_buf[0]
             if segment.get_seq() != self._copied_nxt:
                 break
-            buf = segment.get_buf()
-            if len(buf) > size - len(data):
-                break
-
-        recover = False
-        if len(self._rcv_queue) >= self._rcv_wnd:
-            recover = True
-
-        data = bytes()
-        start = 0
-        for start in range(len(self._rcv_queue)):
-            segment = self._rcv_queue[start]
-            data += segment.get_data()
-            if segment.get_frg() == 0:
-                break
-        if ispeek:
-            self._rcv_queue = self._rcv_queue[start:]
-
-        while len(self._rcv_buf) > 0:
-            segment = self._rcv_buf[0]
-            if segment.get_sn() != self._rcv_nxt:
-                break
-            if len(self._rcv_queue) >= self._rcv_wnd:
-                break
-            self._rcv_queue.append(segment)
-            self._rcv_buf.pop(0)
-            self._rcv_nxt = u32_next(self._rcv_nxt)
-
-        if recover and self._rcv_queue < self._rcv_wnd:
-            self._probe |= KCP_ASK_TELL
-
+            data += segment.read_buf(size - len(data))
+            if segment.buf_len() <= 0:
+                self._rcv_buf.pop(0)
+                self._copied_nxt = u32_next(self._copied_nxt)
         return data
 
-    def peeksize(self):
-        if len(self._rcv_queue) <= 0:
-            return -1
-        segment = self._rcv_queue[0]
-        if segment.get_frg() == 0:
-            return len(segment.get_data())
-        if len(self._rcv_queue) < segment.get_frg() + 1:
-            return -1
-
-        size = 0
-        for segment in self._rcv_queue:
-            size += len(segment.get_data())
-            if segment.get_frg() == 0:
-                break
-        return size
-
     def send(self, data):
-        if self._stream:
-            if len (self._snd_queue) > 0:
-                segment = self._snd_queue[0]
-                if len(segment.get_data()) < self._mss:
-                    capacity = self._mss - len(segment.get_data())
-                    segment.append_data(data[:capacity])
-                    data = data[capacity:]
-                    if len(data) <= 0:
-                        return 0
-
-        count = 0
-        if len(data) <= self._mss:
-            count = 1
-        else:
-            count = (len + self._mss - 1) / self._mss
-
-        if count > 255:
-            return -2
-        for i in range(count):
-            segment = KcpSegment()
-            segment.set_frg(0 if self._stream else (count - 1 - i))
-            segment.append_data(data[:self._mss])
-            data = data[self._mss:]
-            self._snd_queue.push(segment)
+        if len(data) <= 0:
+            return 0
+        copied_len = 0
+        while True:
+            if len(self._snd_buf) <= 0:
+                break
+            segment = self._snd_buf[-1]
+            if u32_diff(segment.get_seq(), self._snd_nxt) < 0:
+                break
+            copied_len = min(len(data), self._mss - segment.buf_len())
+            if copied_len <= 0:
+                copied_len = 0
+                break
+            segment.append_buf(data[:copied_len])
+            break
+        while copied_len < len(data) and u32_diff()
         return 0
 
     def update_ack(self, rtt):
